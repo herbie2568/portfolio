f9856c2d9bb43cb6c64860e23baaa41497d4de41
Lily Chen <chenlily1998@gmail.com>
2022-01-27 12:32:04 -0500

    added anchors

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ $('<h1>').text('LILY CHEN').appendTo('header').addClass('header').attr('id', 'he
 
 /////CAROUSEL/////
 const $imgBar = $('<div>').appendTo('body').addClass('imgbar')
-const $lilyPics = $('<div>').addClass('lilypics-div').appendTo($imgBar)
-$(`<img src ='lilypics/IMG_3661.jpeg'>`).appendTo($lilyPics)
+$('<div>').addClass('lilypics-div').appendTo($imgBar)
+$(`<img src ='lilypics/IMG_3661.jpeg'>`).appendTo('.lilypics-div')
 // $('<div>').addClass('lilypics-div').appendTo($imgBar)
 // $(`<img src ='lilypics/IMG_3661.jpeg'>`).appendTo($lilyPics)
 // $('<div>').addClass('lilypics-div').appendTo($imgBar)
@@ -20,11 +20,25 @@ $(`<img src ='lilypics/IMG_3661.jpeg'>`).appendTo($lilyPics)
 ////NAV BAR/////
 const $navBar = $('<div>').addClass('navbar-div').appendTo('.imgbar')
 $('<ul>').appendTo($navBar).addClass('nav-bar')
-$('<li>').appendTo('.nav-bar').text('HOME').addClass('nav-list')
-$('<li>').appendTo('.nav-bar').text('BIO').addClass('nav-list')
-$('<li>').appendTo('.nav-bar').text('RESUME').addClass('nav-list')
-$('<li>').appendTo('.nav-bar').text('PROJECTS').addClass('nav-list')
-$('<li>').appendTo('.nav-bar').text('LINKS').addClass('nav-list')
+$('<li>').appendTo('.nav-bar').addClass('nav-list').attr('id', 'home-li')
+$('<a href= #home>').appendTo('#home-li').text('HOME').addClass('nav-anchor')
+$('<li>').appendTo('.nav-bar').addClass('nav-list').attr('id', 'bio-li')
+$('<a href= #bio>').appendTo('#bio-li').text('BIO').addClass('nav-anchor')
+$('<li>').appendTo('.nav-bar').addClass('nav-list').attr('id', 'resume-li')
+$('<a href= #resume>').appendTo('#resume-li').text('RESUME').addClass('nav-anchor')
+$('<li>').appendTo('.nav-bar').addClass('nav-list').attr('id', 'projects-li')
+$('<a href= #projects>').appendTo('#projects-li').text('PROJECTS').addClass('nav-anchor')
+$('<li>').appendTo('.nav-bar').addClass('nav-list').attr('id', 'links-li')
+$('<a href= #links>').appendTo('#links-li').text('LINKS').addClass('nav-anchor')
 
+//////SECTIONS//////
+$('<div>').addClass('stuff').appendTo('body')
+$('<section>').attr('id', 'bio').appendTo('.stuff')
+$('<section>').attr('id', 'resume').appendTo('.stuff')
+$('<section>').attr('id', 'projects').appendTo('.stuff')
+$('<section>').attr('id', 'links').appendTo('.stuff')
+
+//////BIO///////
+$('<p>').appendTo('#bio').text('Hi, I\'m Lily! I\'m a software engineer based in Brooklyn, NY. Feel free to check out my stuff in the links!').addClass('bio-text')
 
 })
