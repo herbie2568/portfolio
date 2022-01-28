fdea7e0dc0091f518512742293ca9d0f3aa03c72
Lily Chen <chenlily1998@gmail.com>
2022-01-28 00:22:10 -0500

    sticky nav bar

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ $('<h1>').text('LILY CHEN').appendTo('header').addClass('header').attr('id', 'he
 
 /////CAROUSEL IMAGES/////
 const $imgBar = $('<div>').appendTo('body').addClass('imgbar').addClass('carousel-images')
-
-$('<div>').appendTo('.carousel-images').addClass('carousel-button').addClass('previous')
-$(`<img src = 'lilypics/left_arrow.png'>`).appendTo('.previous')
-$('<div>').addClass('carousel-container').appendTo($imgBar).addClass('lilypics-div')
+$('<div>').addClass('arrow-img').appendTo($imgBar)
+// $('<div>').appendTo('.carousel-images').addClass('carousel-button').addClass('previous')
+// $(`<img src = 'lilypics/left_arrow.png'>`).appendTo('.previous')
+$('<div>').appendTo($imgBar).addClass('lilypics-div')
 
 ///central park pic//
 $(`<img src ='lilypics/IMG_3661.jpeg'>`).appendTo('.lilypics-div').addClass('carousel-images')
@@ -24,10 +24,11 @@ $(`<img src ='lilypics/IMG_8047.HEIC.jpg'>`).appendTo('.lilypics-div').addClass(
 $(`<img src ='lilypics/IMG_8509.HEIC.jpg'>`).appendTo('.lilypics-div').addClass('carousel-images')
 //madison sq park//
 $(`<img src ='lilypics/IMG_8826.HEIC.jpg'>`).appendTo('.lilypics-div').addClass('carousel-images')
-$('<div>').appendTo('.carousel-images').addClass('carousel-button').addClass('next')
-$(`<img src = 'lilypics/right_arrow.png'>`).appendTo('.next')
+// $('<div>').appendTo('.carousel-images').addClass('carousel-button').addClass('next')
+// $(`<img src = 'lilypics/right_arrow.png'>`).appendTo('.next')
 
 ////CAROUSEL JQUERY////
+const $carousel = () => {
 currentImgIndex = 0
   numOfImages= $('.carousel-images').children().length - 1
   $('.next').on('click', ()=> {
@@ -50,6 +51,8 @@ currentImgIndex = 0
     $('.carousel-images').children().eq(currentImgIndex).css('display', 'block')
   })
 
+}
+
 ////NAV BAR/////
 const $navBar = $('<div>').addClass('navbar-div').appendTo('.imgbar')
 $('<ul>').appendTo($navBar).addClass('nav-bar')
@@ -61,6 +64,8 @@ $('<li>').appendTo('.nav-bar').addClass('nav-list').attr('id', 'resume-li')
 $('<a href= #resume>').appendTo('#resume-li').text('resume').addClass('nav-anchor')
 $('<li>').appendTo('.nav-bar').addClass('nav-list').attr('id', 'projects-li')
 $('<a href= #projects>').appendTo('#projects-li').text('projects').addClass('nav-anchor')
+$('<li>').appendTo('.nav-bar').addClass('nav-list').attr('id', 'gallery-li')
+$('<a href= #gallery>').appendTo('#gallery-li').text('gallery').addClass('nav-anchor')
 $('<li>').appendTo('.nav-bar').addClass('nav-list').attr('id', 'links-li')
 $('<a href= #links>').appendTo('#links-li').text('links').addClass('nav-anchor')
 
@@ -69,9 +74,31 @@ $('<div>').addClass('stuff').appendTo('body')
 $('<section>').attr('id', 'about').appendTo('.stuff')
 $('<section>').attr('id', 'resume').appendTo('.stuff')
 $('<section>').attr('id', 'projects').appendTo('.stuff')
+$('<section>').attr('id', 'gallery').appendTo('.stuff')
 $('<section>').attr('id', 'links').appendTo('.stuff')
 
+
+
 //////about///////
-$('<p>').appendTo('#about').text('Hi, I\'m Lily! I\'m a software engineer based in Brooklyn, NY. Feel free to check out my stuff in the links!').addClass('about-text')
+$('<h2>').appendTo('#about').text('about')
+$('<p>').appendTo('#about').text('Hi, I\'m Lily! I\'m a software engineer based in Brooklyn, NY. I enjoy doing front end and back end development. My hobbies include swimming, biking, playing the piano/cello, and solving Rubik\'s cubes. I\'m also a bit of a Jeopardy nerd and I watch it religiously. I love learning new things and am constantly honing my current skills. Feel free to check out my random stuff in the links!').addClass('about-text')
+
+
+////resume//////
+$('<h2>').appendTo('#resume').text('resume')
+$('<p>').appendTo('#resume').text('Hi, I\'m Lily! I\'m a software engineer based in Brooklyn, NY. Feel free to check out my stuff in the links!').addClass('resume-text')
+
+
+///projects////
+$('<h2>').appendTo('#projects').text('projects')
+$('<p>').appendTo('#projects').text('Hi, I\'m Lily! I\'m a software engineer based in Brooklyn, NY. Feel free to check out my stuff in the links!').addClass('projects-text')
+
+////gallery////
+$('<h2>').appendTo('#gallery').text('gallery')
+$('<p>').appendTo('#gallery').text('Hi, I\'m Lily! I\'m a software engineer based in Brooklyn, NY. Feel free to check out my stuff in the links!').addClass('gallery-text')
+
+////links////
+$('<h2>').appendTo('#links').text('links')
+$('<p>').appendTo('#links').text('Hi, I\'m Lily! I\'m a software engineer based in Brooklyn, NY. Feel free to check out my stuff in the links!').addClass('links-text')
 
 })
